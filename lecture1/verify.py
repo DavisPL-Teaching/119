0ba344b7a3fe36dd29471dcce5fce97af4ca01b6
Caleb Stanford <cdstanford@ucdavis.edu>
2024-09-26 19:00:03 -0700

    :test_tube: update

--- lecture1/verify.py
+++ lecture1/verify.py
@@ -7,20 +7,23 @@ def verify():
     print("Python version:", sys.version)
     print("Numpy version:", np.__version__)
     print("Pandas version:", pd.__version__)
+    print("Matplotlib version:", plt.matplotlib.__version__)
 
     # Simple numpy and pandas operations to verify functionality
+    # - Create a numpy array
     array = np.array([1, 2, 3])
     print("Numpy array:", array)
 
-    # Add stuff to the array
+    # - Add stuff to the array
     array = array + 4
     print("Numpy array after addition:", array)
 
+    # - Make a pandas DataFrame
     df = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
     print("Pandas DataFrame:\n", df)
 
-    # Make a plot using matplotlib
-    plt.plot(array)
+    # Plot the DataFrame using matplotlib
+    plt.plot(df)
     plt.show()
 
 if __name__ == "__main__":
